fe65a3904806004d4bd9c7c46756ccc0d44f2fa5
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-05-14 00:11:22 +0000

    Localisation updates for special page aliases for extensions from http://translatewiki.net.

--- ApprovedRevs.alias.php
+++ ApprovedRevs.alias.php
@@ -11,6 +11,31 @@ $specialPageAliases['en'] = array(
 	'ApprovedRevs' => array( 'ApprovedRevs', 'ApprovedPages', 'UnapprovedPages' ),
 );
 
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'ApprovedRevs' => array( 'مراجعات_موفقة', 'صفحات_موفقة', 'صفحات_غير_موفقة' ),
+);
+
+/** Esperanto (Esperanto) */
+$specialPageAliases['eo'] = array(
+	'ApprovedRevs' => array( 'Aprobitaj_revizioj' ),
+);
+
+/** 湘语 (湘语) */
+$specialPageAliases['hsn'] = array(
+	'ApprovedRevs' => array( '受认定版本', '受认定页面', '冇认定个页面' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'ApprovedRevs' => array( 'RevAprouve', 'PajAprouve', 'PajPaAprouve' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'ApprovedRevs' => array( 'ОдобрениРевизии' ),
+);
+
 /** Malayalam (മലയാളം) */
 $specialPageAliases['ml'] = array(
 	'ApprovedRevs' => array( 'അംഗീകൃതതാളുകൾ', 'അംഗീകൃതമല്ലാത്തതാളുകൾ' ),
@@ -21,9 +46,19 @@ $specialPageAliases['nl'] = array(
 	'ApprovedRevs' => array( 'GoedgekeurdePaginas', 'GoedgekeurdePagina\'s' ),
 );
 
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬) */
+$specialPageAliases['no'] = array(
+	'ApprovedRevs' => array( 'Godkjente_revisjoner' ),
+);
+
 /** Sanskrit (संस्कृत) */
 $specialPageAliases['sa'] = array(
 	'ApprovedRevs' => array( 'अंगीकृत_पृष्टानि' ),
+);
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = array(
+	'ApprovedRevs' => array( 'OnaylanmışRevizyonlar', 'OnaylanmışSayfalar', 'OnaylanmamışSayfalar' ),
 );
 
 /**
