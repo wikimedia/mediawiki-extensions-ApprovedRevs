1a1ae59b70f6d585c2838d05696950dc1d8d5a0e
Yaron Koren <yaron@users.mediawiki.org>
2012-01-05 19:08:16 +0000

    New version: 0.6

--- ApprovedRevs.php
+++ ApprovedRevs.php
@@ -9,7 +9,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
  * @author Yaron Koren
  */
 
-define( 'APPROVED_REVS_VERSION', '0.5.9' );
+define( 'APPROVED_REVS_VERSION', '0.6' );
 
 // credits
 $wgExtensionCredits['other'][] = array(
