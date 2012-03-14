173d0111204b96cac0a8890d47fe62f31f5493c1
Yaron Koren <yaron@users.mediawiki.org>
2012-03-14 13:35:13 +0000

    Changed "int(8)" to just "int" - specifying number of bytes isn't necessary, and causes problems with PostgreSQL

--- ApprovedRevs.sql
+++ ApprovedRevs.sql
@@ -1,6 +1,6 @@
 CREATE TABLE /*_*/approved_revs (
-	page_id int(8) default NULL,
-	rev_id int(8) default NULL
+	page_id int default NULL,
+	rev_id int default NULL
 ) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX approved_revs_page_id ON /*_*/approved_revs (page_id);
