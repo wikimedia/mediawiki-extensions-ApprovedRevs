f885e61edd2d72f47ee28e797d95877d43962b18
yaron <yaron57@gmail.com>
2012-09-18 21:42:23 +0400

    New version: 0.6.3
    Change-Id: I31bbc67ef41b9aa610a9ddd4668bcd4e185b6bac

--- ApprovedRevs.php
+++ ApprovedRevs.php
@@ -9,7 +9,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
  * @author Yaron Koren
  */
 
-define( 'APPROVED_REVS_VERSION', '0.6.2' );
+define( 'APPROVED_REVS_VERSION', '0.6.3' );
 
 // credits
 $wgExtensionCredits['other'][] = array(
