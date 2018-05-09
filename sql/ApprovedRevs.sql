8a75862cdc8abbe4cf8a15ba5bdf44edaa56ea69
Yaron Koren <yaron57@gmail.com>
2018-05-09 16:06:34 +0000

    Fix for dac770464302 for PostgreSQL DBs
    Really this should be done via another patch, but given that this was just six weeks ago and Postgres is not that popular for MediaWiki, hopefully this is good enough.
    
    Change-Id: I70ef010594a1bf8e0dd08983abe80c00c9881b91

--- sql/ApprovedRevs.sql
+++ sql/ApprovedRevs.sql
@@ -1,7 +1,7 @@
 CREATE TABLE /*_*/approved_revs (
 	page_id int default NULL,
 	rev_id int default NULL,
-	approver_id int unsigned NOT NULL default 0
+	approver_id int NOT NULL default 0
 ) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX approved_revs_page_id ON /*_*/approved_revs (page_id);
--- sql/patch-approver_id.sql
+++ sql/patch-approver_id.sql
@@ -1,2 +1,2 @@
 ALTER TABLE /*_*/approved_revs
-  ADD COLUMN approver_id  int unsigned NOT NULL default 0;
+  ADD COLUMN approver_id int NOT NULL default 0;
