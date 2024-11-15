680c23559f1fad6e7c7cc288d559dcbc3df4de0a
Umherirrender <umherirrender_de.wp@web.de>
2024-11-15 13:48:27 +0100

    Use explicit nullable type on parameter arguments (for PHP 8.4)
    Implicitly marking parameter $... as nullable is deprecated in PHP
    8.4. The explicit nullable type must be used instead.
    
    Change-Id: I3e4274285018db9edb28aa87c53889a1fe8a3f20

--- includes/ARParserFunctions.php
+++ includes/ARParserFunctions.php
@@ -125,23 +125,23 @@ class ARParserFunctions {
 		return date( $formatString, $approvalInfo[0] );
 	}
 
-	public static function renderApprovalYear( Parser &$parser, string $pageName = null ) {
+	public static function renderApprovalYear( Parser &$parser, ?string $pageName = null ) {
 		return self::formatApprovalDate( $parser, $pageName, 'Y' );
 	}
 
-	public static function renderApprovalMonth( Parser &$parser, string $pageName = null ) {
+	public static function renderApprovalMonth( Parser &$parser, ?string $pageName = null ) {
 		return self::formatApprovalDate( $parser, $pageName, 'm' );
 	}
 
-	public static function renderApprovalDay( Parser &$parser, string $pageName = null ) {
+	public static function renderApprovalDay( Parser &$parser, ?string $pageName = null ) {
 		return self::formatApprovalDate( $parser, $pageName, 'd' );
 	}
 
-	public static function renderApprovalTimestamp( Parser &$parser, string $pageName = null ) {
+	public static function renderApprovalTimestamp( Parser &$parser, ?string $pageName = null ) {
 		return self::formatApprovalDate( $parser, $pageName, 'YmdHis' );
 	}
 
-	public static function renderApprovalUser( Parser &$parser, string $pageName = null ) {
+	public static function renderApprovalUser( Parser &$parser, ?string $pageName = null ) {
 		if ( $pageName !== null && $pageName !== '' ) {
 			$title = Title::newFromText( $pageName );
 		} else {
