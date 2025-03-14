a6a2edbde48db0094eb982c268781d12aecfbdcc
Yaron Koren <yaron57@gmail.com>
2025-03-14 13:59:36 +0000

    Further removal of pre-MW 1.40 compatibility
    Change-Id: I6afe574419c5c86bd3669d1754c21b9ed968c5a6

--- includes/ApprovedRevs.php
+++ includes/ApprovedRevs.php
@@ -206,26 +206,18 @@ class ApprovedRevs {
 	 * @return IReadableDatabase
 	 */
 	public static function getReadDB() {
-		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		if ( method_exists( $lbFactory, 'getReplicaDatabase' ) ) {
-			// MW 1.40+
-			return $lbFactory->getReplicaDatabase();
-		} else {
-			return $lbFactory->getMainLB()->getConnection( DB_REPLICA );
-		}
+		return MediaWikiServices::getInstance()
+			->getDBLoadBalancerFactory()
+			->getReplicaDatabase();
 	}
 
 	/**
 	 * @return IDatabase
 	 */
 	public static function getWriteDB() {
-		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		if ( method_exists( $lbFactory, 'getPrimaryDatabase' ) ) {
-			// MW 1.40+
-			return $lbFactory->getPrimaryDatabase();
-		} else {
-			return $lbFactory->getMainLB()->getConnection( DB_PRIMARY );
-		}
+		return MediaWikiServices::getInstance()
+			->getDBLoadBalancerFactory()
+			->getPrimaryDatabase();
 	}
 
 	/**
